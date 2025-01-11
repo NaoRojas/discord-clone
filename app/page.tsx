845557a0a5597b0ae7922ad984314fcd1e50ba31
naoreds <naorojash@gmail.com>
2025-01-10 23:26:22 -0600

    Update layout and home page with new font and improved structure; add reusable Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <p className="text-2xl font-bold text-indigo-500">Welcome</p>
+  return (
+    <div className="flex p-10 center">
+      <div className="flex flex-col center">
+        <h1 className="text-4xl font-bold">Welcome to your new app!</h1>
+        <p className="mt-4 text-lg">
+          This is your new app. It's ready to go out of the box. Just start
+          building!
+        </p>
+      </div>
+    </div>
+  )
 }
