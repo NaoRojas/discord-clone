936527a90500a45e8196d27cbf9a1feb8863410c
naoreds <naorojash@gmail.com>
2025-01-11 01:14:16 -0600

    Add theme toggle functionality with dropdown menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 const openSans = Open_Sans({
   subsets: ['latin'],
@@ -20,7 +21,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${openSans.className}  antialiased`}>{children}</body>
+        <body className={`${openSans.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
