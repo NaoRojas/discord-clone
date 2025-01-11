e2391b54d893b2324bf251e2083ae3a4fd46cbc0
naoreds <naorojash@gmail.com>
2025-01-11 01:16:47 -0600

    Add utility function for conditional class names in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const openSans = Open_Sans({
   subsets: ['latin'],
@@ -21,7 +22,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${openSans.className}`}>
+        <body
+          className={cn(`${openSans.className}`, 'bg-white dark:bg-[#313338')}
+        >
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
